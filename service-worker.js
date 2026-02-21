12ca4a13b9053f537cbc71429d864526d89ff958
mark brian angco <86399572+markb3322@users.noreply.github.com>
2026-02-21 21:16:57 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,21 @@
 const CACHE_NAME = 'mcb-v1';
-const assets = ['index.html', 'mcb.png'];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// I-cache lang ang mga files na sigurado kang nandun
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html'
+            ]).catch(err => console.log("Skip caching optional files"));
+        })
+    );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            return res || fetch(e.request);
+        })
+    );
 });
